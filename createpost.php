01e11b5d76bc926346761b1cb5b142502267246e
Lilly05 <lilly.koller@icloud.com>
2021-11-24 16:17:35 +0100

    sql injection safe

--- createpost.php
+++ createpost.php
@@ -5,7 +5,6 @@ $password = '';
 $database = 'wordpress';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-try {
     $name = $_POST['name'] ?? '';
     $title = $_POST['title'] ?? '';
     $post = $_POST['post'] ?? '';
@@ -13,16 +12,9 @@ try {
     $conn = new PDO('mysql:host=localhost;dbname=' . $database, $user, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "INSERT INTO posts (created_by, post_title, post_text)
-    VALUES ('$name', '$title', '$post')";
-    // use exec() because no results are returned
-    $conn->exec($sql);
-    echo "New record created successfully";
-  } catch(PDOException $e) {
-    echo $sql . "<br>" . $e->getMessage();
-  }
-  
-  $conn = null;
+    $sql = $conn -> prepare("INSERT INTO posts (created_by, post_title, post_text) VALUES (:name, :title, :post)");
+    
+    $sql->execute([':name' => $name, ':title' => $title, ':post' => $post]);
 }
 ?>
 
