07eb2dbbdea5e3bc3e21234e064124188f5e02c8
Lilly05 <lilly.koller@icloud.com>
2021-12-01 19:23:40 +0100

    added comments

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <?php
 
 $pdo = new PDO('mysql:host=localhost;dbname=blog', 'root', '');
-$sql = "SELECT created_by, created_at, post_title, link, post_text, comments FROM posts ORDER BY created_at DESC";
+$sql = "SELECT * FROM posts ORDER BY created_at DESC";
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $comment = $_POST['comment'] ?? '';
+if(isset($_POST['comment-id'])) {
+    $idComment  = $_POST['comment-id'];
+    $comment = $_POST['comment_block'] ?? '';
     $comment = htmlspecialchars($comment);
+    $stmt = $pdo->prepare('INSERT INTO comments (comment, blog_id)
+    VALUES(:comment, :id)');
+    $stmt->execute([':comment' => $comment, ':id' => $idComment]);
+ }  
 
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql2 = $pdo -> prepare("INSERT INTO posts (comments) VALUES (:comment)");
-
-    $sql2->execute([':comment' => $comment]);
-    }
+ $stmt = $pdo->query('SELECT * FROM `comments`');
+ $comments = $stmt-> fetchAll();
 ?>
 
 <!DOCTYPE html>
@@ -30,32 +32,44 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         ?>
         <main class="background">
             <?php
-            foreach ($pdo->query($sql) as $row) { 
-            $link = $row['link'];?>
-
-            <h2 class="title"> <?php
-            echo $row['created_by'];?> 
-            </h2> <p class="time"> <?php
-            echo $row['created_at']."<br>"; ?> 
-            <h5 class="title"> </p><?php
-            echo $row['post_title']."<br>"; ?>
-            </h5><?php
-            echo "<img src='" . $link . "' . class='image'><br>";
-            ?></p>
-            <p class= "post"> <?php
-            echo $row['post_text']."<br>"; ?>
-            </p> 
-            <form method="POST">
-            <label for="comment">Kommentar:</label><br>
-            <input type="text" name="comment" value="<?=$comment ?? ''?>"><br><br>
-            <div class="border">
-            <input class="comment" type="submit" value="Posten"><br><br>
-            </div>
-            </form>
-            <?php
-            }
+            foreach ($pdo->query($sql) as $post) { 
+                $link = $post['link'];
+                ?>
+                <div class="post-box">
+                    <div class="post">
+                        <h2 class="title"> <?= $post['created_by'] ?></h2> 
+                        <p class="time"> <?= $post['created_at'] ?></p>
+                        <h5 class="title"> <?= $post['post_title']."<br>" ?></h5>
+                        <p><img src="<?= $link ?>" class="image"></p>
+                        <p class= "post"> <?= $post['post_text'] ?></p>
+                    </div>
+                    <div class="comments-box">
+                        <div class="add-comment-box">
+                            <form method="POST">
+                                <label for="comment_block">Neuer Kommentar:</label><br><br>
+                                <input type="text" name="comment_block"?><br><br>
+                                <input type="submit" value="Posten"><br><br>
+                                <input type="hidden" name="comment-id" value="<?=$post['blog_id']?>">
+                            </form>
+                        </div>
+                        <div class="comments">
+                            <h5>Kommentare</h5>
+                            <?php foreach($comments as $comment): ?>
+                                <?php if($comment['blog_id'] === $post['blog_id']): ?>
+                                    <?php if($comment !== " "): ?>
+                                        <div class="comment">
+                                            <?= $comment['comment'] ?>
+                                        </div>
+                                    <?php endif; ?>
+                                <?php endif; ?>
+                            <?php endforeach; ?>     
+                        </div>
+                     </div>
+                </div> <!-- /post-box -->
+                <?php 
+                } 
             ?>
         </main>
     </div>
 </body>
-<html>
+</html>
